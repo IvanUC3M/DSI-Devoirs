d8cfaf8b22d515bbad5848f8233155250110efa6
Satsarof <garmciv@gmail.com>
2018-04-27 12:04:10 +0200

    commit archivos
    Todos los archivos

---
+++ PRACTICA_FINAL/devoirs/src/pages/Gastos/Gastos.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-gastos',
+  templateUrl: 'Gastos.html'
+})
+export class GastosPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ PRACTICA_FINAL/devoirs/src/pages/Horario/Horario.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-horario',
+  templateUrl: 'Horario.html'
+})
+export class HorarioPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
